632ef3954f395e4e2826d592a0bfd321de1f131b
Madhuranga Bandara <madurangabandara2346@gmail.com>
2024-11-26 08:46:46 +0000

    component importing bug fixed

--- day8/src/App.js
+++ day8/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import {BrowserRouter,Router,Routes,Link,NavLink, Route} from "react-router-dom";
-import {Home} from "./components/Home";
-import {AboutUs} from "./components/AboutUs";
-import {ContactUs} from "./components/ContactUs";
+import Home from "./components/Home";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
 
 function App() {
   return (
